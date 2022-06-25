371fe8e7230318e50e7a95a3d62ba367aa3db80c
Mehmet Ugur SATIR <musatir@gmail.com>
2022-06-25 22:34:31 +0300

    pause added

--- app2.js
+++ app2.js
@@ -4,14 +4,28 @@ const resetBtn = document.getElementById('reset');
 
 let startTime;
 let pauseTime;
-let diff;
+let diff = 0;
+let startStopwatch;
 
+function msConverter(ms){
+    const seconds = ms / 1000;
+    const hour = Math.floor(seconds/(60*60));
+    const remainder = seconds % (60*60);
+    const min = Math.floor(remainder/60);
+    const sec = Math.floor(remainder % 60);
+    document.getElementById("timer").innerText = `${hour} : ${min} : ${sec}`;
+}
 startBtn.addEventListener('click', ()=>{
     startTime = new Date();
+    startStopwatch = setInterval(()=>{
+        msConverter(diff + (new Date() - startTime));
+    }, 1000)
 })
 
 pauseBtn.addEventListener('click', ()=>{
     pauseTime = new Date();
-    diff = (pauseTime - startTime);
-    console.log(diff.to);
+    diff += (pauseTime - startTime);
+    clearInterval(startStopwatch);
 })
+
+//console.log(msConverter(107012350));
