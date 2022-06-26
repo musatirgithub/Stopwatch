87573ddcfb332297971606aab4c0821e50e3c3e3
Mehmet Ugur SATIR <musatir@gmail.com>
2022-06-26 09:57:46 +0300

    reset button

--- app2.js
+++ app2.js
@@ -6,26 +6,44 @@ let startTime;
 let pauseTime;
 let diff = 0;
 let startStopwatch;
+let seconds;
+let hour;
+let remainder;
+let min;
+let sec;
 
 function msConverter(ms){
-    const seconds = ms / 1000;
-    const hour = Math.floor(seconds/(60*60));
-    const remainder = seconds % (60*60);
-    const min = Math.floor(remainder/60);
-    const sec = Math.floor(remainder % 60);
+    seconds = ms / 1000;
+    hour = Math.floor(seconds/(60*60));
+    remainder = seconds % (60*60);
+    min = Math.floor(remainder/60);
+    sec = Math.floor(remainder % 60);
     document.getElementById("timer").innerText = `${hour} : ${min} : ${sec}`;
 }
 startBtn.addEventListener('click', ()=>{
     startTime = new Date();
+    clearInterval(startStopwatch);
     startStopwatch = setInterval(()=>{
         msConverter(diff + (new Date() - startTime));
     }, 1000)
 })
 
 pauseBtn.addEventListener('click', ()=>{
-    pauseTime = new Date();
-    diff += (pauseTime - startTime);
-    clearInterval(startStopwatch);
+    
+    if (startTime != null) {
+        pauseTime = new Date();
+        diff += (pauseTime - startTime);
+        clearInterval(startStopwatch);
+    } 
 })
 
+resetBtn.addEventListener('click', ()=>{
+    startTime = null;
+    pauseTime = null;
+    hour = 0;
+    min = 0;
+    sec = 0;
+    document.getElementById("timer").innerText = `${hour} : ${min} : ${sec}`;
+clearInterval(startStopwatch);
+})
 //console.log(msConverter(107012350));
